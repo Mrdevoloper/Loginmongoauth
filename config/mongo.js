b8c332f93cc4a9c0185a6927b22eb3598599edd6
Mrdevoloper <nasibullayevx177@gmail.com>
2022-09-21 13:25:37 -0600

    chotki

--- config/mongo.js
+++ config/mongo.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 const mongo = async() => {
     try {
-        return await mongoose.connect('mongodb://localhost:27017/eshmat')
+        return await mongoose.connect('mongodb+srv://superUser:xayrullo2003@newcluster.1pce53g.mongodb.net/test')
     } catch (error) {
         console.log(error.message);
     }
