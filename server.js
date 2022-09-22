6c9736898247312a9e4300d6b1714f9e67b75d91
Mrdevoloper <nasibullayevx177@gmail.com>
2022-09-22 01:51:51 -0600

    swagger

--- server.js
+++ server.js
@@ -3,9 +3,18 @@ const { mongo } = require('./config/mongo');
 const app = express();
 const UserRouter = require('./Api/User');
 const PORT = process.env.PORT || 9000;
-
+const swaggerUi = require('swagger-ui-express');
+const swaggerJSDoc = require('swagger-jsdoc');
+const options = require('./swagger')
 app.use(express.json());
 app.use('/user', UserRouter);
+
+
+
+const swaggerSpec = swaggerJSDoc(options);
+app.use('/user/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
+
 
 mongo()
 	.then(() => console.log('connected'))
---
+++ swagger.js
@@ -0,0 +1,14 @@
+
+ const opitons = {
+	swaggerDefinition: {
+		info: {
+			title: 'Node Js Authentification API for mogodb',
+			version: '1.0.0',
+		},
+	},
+	apis: ['./Api/User'],
+}
+
+
+
+module.exports = opitons
