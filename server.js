d0d6c4b39d84e913a2d7184384adad8fedbbd915
Mrdevoloper <nasibullayevx177@gmail.com>
2022-09-22 02:16:16 -0600

    swagger-all

--- server.js
+++ server.js
@@ -4,15 +4,12 @@ const app = express();
 const UserRouter = require('./Api/User');
 const PORT = process.env.PORT || 9000;
 const swaggerUi = require('swagger-ui-express');
-const swaggerJSDoc = require('swagger-jsdoc');
 const options = require('./swagger')
+
+
 app.use(express.json());
 app.use('/user', UserRouter);
-
-
-
-const swaggerSpec = swaggerJSDoc(options);
-app.use('/user/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+app.use('/user/api-docs', swaggerUi.serve, swaggerUi.setup(options));
 
 
 
